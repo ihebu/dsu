14ce3b682ca06712112d0488cbd220e30e954646
Iheb Haboubi <iheb.haboubi56@gmail.com>
2021-04-27 21:35:28 +0100

    add comments in code

--- dsu.go
+++ dsu.go
@@ -1,3 +1,15 @@
+// Implementation of the Disjoint-Set data structure.
+//
+// Also called a Union-Find or Merge-Find set,
+// is a data structure that stores a collection of disjoint (non-overlapping) sets.
+// Equivalently, it stores a partition of a set into disjoint subsets.
+// It provides operations for adding new sets,
+// merging sets (replacing them by their union),
+// and finding a representative member of a set.
+// The last operation allows to find out efficiently if any two elements are in the same or different sets.
+//
+// more at https://en.wikipedia.org/wiki/Disjoint-set_data_structure
+
 package dsu
 
 type node struct {
@@ -6,20 +18,25 @@ type node struct {
 	size   int
 }
 
+// DSU is the type used to the Disjoint Set data structure.
+// it maps from a value to a node pointer corresponding to the element in the set.
 type DSU struct {
 	nodes map[interface{}]*node
 }
 
+// New returns a pointer to an empty initialized DSU instance.
 func New() *DSU {
 	return &DSU{map[interface{}]*node{}}
 }
 
+// Contains checks if a given element is present in the disjoint set.
 func (d *DSU) Contains(x interface{}) bool {
 	_, ok := d.nodes[x]
 
 	return ok
 }
 
+// Add takes an element as a parameter and inserts it in the disjoint set.
 func (d *DSU) Add(x interface{}) bool {
 	if d.Contains(x) {
 		return false
@@ -29,6 +46,8 @@ func (d *DSU) Add(x interface{}) bool {
 	return true
 }
 
+// Find returns the root element that represents the set to which x belongs to.
+// If the element doesn't exist in the set, Find returns the nil value.
 func (d *DSU) Find(x interface{}) interface{} {
 	if !d.Contains(x) {
 		return nil
@@ -49,6 +68,13 @@ func (d *DSU) Find(x interface{}) interface{} {
 	return node.parent.value
 }
 
+// Union replaces the set containing x and the set containing y with their union.
+// Union uses Find to determine the roots of the trees containing x and y.
+// If the roots are the same of one of the elements doesn't exist in the set,
+// there is nothing more to do. and Union returns false
+// Otherwise, the two sets get be merged. This is done by either setting
+// the parent element of the element with the smaller size to the other parent
+// and the return of the function in this case is true
 func (d *DSU) Union(x, y interface{}) bool {
 	if !d.Contains(x) || !d.Contains(y) {
 		return false
