65fdf7a64addbb09cec693e28ef42dccbe680c23
Iheb Haboubi <iheb.haboubi56@gmail.com>
2021-04-27 16:26:02 +0100

    add test for Contains() method

--- dsu_test.go
+++ dsu_test.go
@@ -1,14 +1,30 @@
 package dsu
 
-import "testing"
+import (
+	"testing"
+)
 
-func assertEqual(t *testing.T, got, expected bool) {
+func assertEqual(t *testing.T, got, expected interface{}) {
 	t.Helper()
 
 	if got != expected {
-		t.Errorf("expected %t, got %t", expected, got)
+		t.Errorf("expected %v, got %v", expected, got)
 	}
 }
+
+func TestContains(t *testing.T) {
+	d := New()
+	node1 := &node{value: 1, parent: nil, size: 1}
+	node2 := &node{value: "foo", parent: nil, size: 1}
+
+	d.nodes[1] = node1
+	d.nodes["foo"] = node2
+
+	assertEqual(t, d.Contains(1), true)
+	assertEqual(t, d.Contains("foo"), true)
+	assertEqual(t, d.Contains(0), false)
+}
+
 func TestAdd(t *testing.T) {
 
 	t.Run("Adding a new element to the set", func(t *testing.T) {
@@ -22,5 +38,4 @@ func TestAdd(t *testing.T) {
 
 		assertEqual(t, d.Add(1), false)
 	})
-
 }
