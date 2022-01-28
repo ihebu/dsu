0c47ab9a3854c714782fd24a9538787213b26595
Iheb Haboubi <iheb.haboubi56@gmail.com>
2022-01-28 21:23:18 +0100

    fix comments

--- dsu.go
+++ dsu.go
@@ -19,7 +19,6 @@ type node struct {
 }
 
 // DSU is the type used to the Disjoint Set data structure.
-// it maps from a value to a node pointer corresponding to the element in the set.
 type DSU struct {
 	nodes map[interface{}]*node
 }
@@ -36,8 +35,8 @@ func (d *DSU) Contains(x interface{}) bool {
 	return ok
 }
 
-// Add takes an element as a parameter and inserts it in the disjoint set.
-// If the element already exists in the set, then nothing is done,
+// Add adds a new element. This element is placed into a new set containing only the new element, and the new set is added to the data structure.
+// If the element already exists in the data structure, then nothing is done,
 // and the return is false
 // otherwise returns true
 func (d *DSU) Add(x interface{}) bool {
@@ -73,7 +72,7 @@ func (d *DSU) Find(x interface{}) interface{} {
 
 // Union replaces the set containing x and the set containing y with their union.
 // Union uses Find to determine the roots of the trees containing x and y.
-// If the roots are the same of one of the elements doesn't exist in the set,
+// If the roots are the same or one of the elements doesn't exist in the set,
 // there is nothing more to do. and Union returns false
 // Otherwise, the two sets get be merged. This is done by either setting
 // the parent element of the element with the smaller size to the other parent
